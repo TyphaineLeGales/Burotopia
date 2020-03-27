2fff7f106f831e66562d586494f11420d93b6ac1
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-03-27 14:48:18 +0100

    generate random open/closed state for desks

--- game.js
+++ game.js
@@ -3,6 +3,7 @@ var id = [];
 var stringID = "";
 var counterIDs = [];
 var idLength = 5;
+var isTheAnswer = false;
 
 
 var playerID = document.querySelector("#playerID");
@@ -51,12 +52,29 @@ function generateCharacter() {
 
 function generateDesksId () {
   // pick a random desk that will have the same id as the player
+  var correctDesk = desks[getRandomInt(desks.length)];
+  correctDesk.classList.add("isTheAnswer");
+  correctDesk.childNodes[1].innerHTML = stringID;
+  correctDesk.childNodes[3].innerHTML = "open";
+
+  for(var i = 0; i< desks.length; i++) {
+    //check for every desks that is not the answer
+    if(desks[i].classList[1] != "isTheAnswer") {
+      var deskState =  desks[i].childNodes[3];
+      var randBinary = Math.round(Math.random());
+      if(randBinary === 0) {
+        deskState.innerHTML = "closed";
+      } else {
+        deskState.innerHTML = "open";
+      }
+
+    }
+  }
+
   // for other desks : generate random variations from the idElement of the player
 
   // var deskId = desks[index].childNodes[1];
   // var deskState = desks[index].childNodes[3];
-  desks[getRandomInt(desks.length)].childNodes[1].innerHTML = stringID;
-
 }
 
 function reset () {
@@ -66,5 +84,9 @@ function reset () {
     var deskState = desks[i].childNodes[3];
     deskState.innerHTML = "";
     deskId.innerHTML = "";
+
+    if(desks[i].classList[1] === "isTheAnswer") {
+      desks[i].classList.remove("isTheAnswer");
+    }
   }
 }
