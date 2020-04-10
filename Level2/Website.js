3710afedfd7bf3f63b8e1266583347bbdeff80f1
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-04-10 17:37:12 +0200

    create metho with input fields fixed

--- Level2/Website.js
+++ Level2/Website.js
@@ -10,7 +10,7 @@ class Website{
   }
 
 
-  createWebsite () {
+  create () {
     var websiteDiv =  document.createElement("div");
     websiteDiv.classList.add("websiteDiv");
 
@@ -18,16 +18,21 @@ class Website{
     topBar.classList.add("topBarSite");
 
     var crossIcon = document.createElement("div");
-    crossIcon.classList.add("crossIcon topBarSquare");
+    crossIcon.classList.add("crossIcon");
+    crossIcon.classList.add("topBarSquare");
+
 
     var minusIcon = document.createElement("div");
-    minusIcon.classList.add("minusIcon topBarSquare");
+    minusIcon.classList.add("minusIcon");
+    minusIcon.classList.add("topBarSquare");
 
     var plusIcon = document.createElement("div");
-    plusIcon.classList.add("plusIcon topBarSquare");
+    plusIcon.classList.add("plusIcon");
+    plusIcon.classList.add("topBarSquare");
 
     var urlBar = document.createElement("div");
-    urlBar.classList.add("urlBar topBarSquare");
+    urlBar.classList.add("urlBar");
+    urlBar.classList.add("topBarSquare");
 
     topBar.appendChild(crossIcon);
     topBar.appendChild(minusIcon);
@@ -37,17 +42,30 @@ class Website{
     var background = document.createElement("div");
     background.classList.add("backgroundSite");
 
-    var inputUsername = document.createElement("div");
-    inputUsername.classList.add("inputFields");
+    var input_1 = document.createElement("div");
+    input_1.classList.add("inputFields");
 
-    var inputPassword = document.createElement("div");
-    inputUsername.classList.add("inputFields");
+    var inputUsername = document.createElement("input");
+    inputUsername.setAttribute("type", "text");
+    inputUsername.setAttribute("placeholder", "Username");
 
-    background.appendChild(inputUsername);
-    background.appendChild(inputPassword);
+    var input_2 = document.createElement("div");
+    input_2.classList.add("inputFields");
+
+    var inputPassword = document.createElement("input");
+    inputPassword.setAttribute("type", "text");
+    inputPassword.setAttribute("placeholder", "Password");
+
+    input_1.appendChild(inputUsername);
+    input_2.appendChild(inputPassword);
+
+    background.appendChild(input_1);
+    background.appendChild(input_2);
 
     websiteDiv.appendChild(topBar);
     websiteDiv.appendChild(background);
+
+    container.appendChild(websiteDiv);
 
   }
 
--- Level2/gameLevel2.js
+++ Level2/gameLevel2.js
@@ -1,3 +1,21 @@
+var button = document.querySelector("button");
+button.style.cursor = "pointer";
+button.addEventListener("click", clickHandler, false);
+
+
+function clickHandler() {
+  playGame();
+}
+
 function playGame() {
+  var testWebsite = new Website();
+  testWebsite.create()
+}
+
+function endGame() {
+
+}
+
+function reset() {
 
 }
