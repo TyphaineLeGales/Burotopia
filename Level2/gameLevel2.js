84b5e7e73447fb0368e1e9946ba033669e7ba533
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-04-10 18:44:48 +0200

    to do list + start screen

--- Level2/gameLevel2.js
+++ Level2/gameLevel2.js
@@ -1,4 +1,7 @@
+var tasks = ["bank", "phoneContract", "streaming", "socialMedia"];
+var taskContainer = document.querySelector("#tasksList");
 var button = document.querySelector("button");
+var gameContainer =  document.querySelector("div.gameContainer");
 button.style.cursor = "pointer";
 button.addEventListener("click", clickHandler, false);
 
@@ -8,12 +11,32 @@ button.addEventListener("click", clickHandler, false);
 // - design layout + websites content
 
 function clickHandler() {
+  gameContainer.style.display = "flex";
+  button.style.display="none";
   playGame();
 }
 
 function playGame() {
-  var testWebsite = new Website();
-  testWebsite.create()
+  //generate listOfTasks
+  //generate an email
+  //several emails ?
+  //on taskItemClick => create corresponding website
+  //in the beginning => player doesn't know his email => needs to check the emailIcon + the player hasn't enter any password => need to click I forgot my password
+  //if userName & passWord
+  //add tasks as the player goes along
+  //possibility to drag and drop windows
+  generateTasks();
+
+}
+
+function generateTasks () {
+  for(var i = 0; i < tasks.length; i++) {
+    var task = tasks[i];
+    var listElement = document.createElement("li");
+    listElement.innerHTML = task;
+    taskContainer.appendChild(listElement);
+
+  }
 }
 
 function endGame() {
