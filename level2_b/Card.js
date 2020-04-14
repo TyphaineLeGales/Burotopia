a21e37e301f0e881fbf03f5dde119dd5462e2224
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-04-14 13:46:50 +0200

    memory game start

---
+++ level2_b/Card.js
@@ -0,0 +1,15 @@
+const container = document.querySelector("div.cardContainer");
+
+class Card {
+  constructor {
+    this.name = null;
+    this.graphics = null;
+    this.password = null;
+  }
+
+  flipCard () {
+
+  }
+}
+
+
---
+++ level2_b/game_2b.js
@@ -0,0 +1,4 @@
+var button = document.querySelector("button");
+var gameContainer =  document.querySelector("div.gameContainer");
+button.style.cursor = "pointer";
+button.addEventListener("click", clickHandler, false);
