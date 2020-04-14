aa4ea4daf2c22de686b728f133cac1084304cecc
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-04-14 15:46:06 +0200

    generate rand grid of pairs

--- level2_b/game_2b.js
+++ level2_b/game_2b.js
@@ -1,9 +1,11 @@
 var button = document.querySelector("button");
 var gameContainer =  document.querySelector("div.gameContainer");
+const cardContainer = document.querySelector("div.cardContainer");
 button.style.cursor = "pointer";
 button.addEventListener("click", clickHandler, false);
 
 var _numberOfCards = 10;
+var cells = [];
 var services = [];
 var passwords = [];
 
@@ -13,6 +15,7 @@ var passwords = [];
 //flip card onclick
 //count tries
 //timer
+//ajouter un post it manuscrit avec noter chaque password pour chaque site
 
 
 function clickHandler() {
@@ -22,7 +25,9 @@ function clickHandler() {
 }
 
 function playGame() {
-  generatePairs();
+  // generatePairs();
+  generateCardGrid();
+  fillGridRand();
 
 }
 
@@ -36,6 +41,31 @@ function generatePairs() {
   }
 }
 
-function fillGridRand() {
+function generateCardGrid() {
+  for(var i=0; i<_numberOfCards*2; i++) {
+    var cell = document.createElement("div")
+    cell.classList.add("cell");
+    cardContainer.appendChild(cell);
+    cells.push(cell);
+  }
+}
 
+function fillGridRand() {
+  console.log(cells);
+  //getRandCell => append pair.service
+   for(var i=0; i<_numberOfCards; i++) {
+    var pair = new Pair(i);
+    pair.create();
+    services.push(pair.serviceCard);
+    passwords.push(pair.passwordCard);
+    var randInt = getRandomInt(cells.length);
+    console.log(randInt);
+    console.log(cells[randInt]);
+    cells[randInt].appendChild(pair.serviceCard);
+    cells.splice(randInt,1);
+    var randInt_2 = getRandomInt(cells.length);
+    cells[randInt_2].appendChild(pair.passwordCard);
+    cells.splice(randInt_2,1);
+    console.log(randInt_2);
+  }
 }
