7a36bc031a2b0420d7b03c632c3cb8ccd408079b
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-05-15 18:46:58 +0200

    unsubscribe game prototype finished

--- level4/unsubscribeGame.js
+++ level4/unsubscribeGame.js
@@ -3,13 +3,57 @@ button.style.cursor = "pointer";
 button.addEventListener("click", clickHandler, false);
 var startUI = document.querySelector(".introLevel");
 var unsubscribe = document.getElementById("unsubscribe");
+unsubscribe.addEventListener('click', win, false);
+var container = document.querySelector('div.gameContainer');
+var endGameMSG = document.querySelector('.endGameMSG');
+var _spacing = 50;
 
 function clickHandler() {
-  console.log("function is called");
+
   playGame();
   startUI.style.display= "none";
 }
 
 function playGame() {
+  generateRandPos();
   unsubscribe.style.display = "block";
+}
+
+function generateRandPos() {
+  var minMarginRight = 100;
+  var minMarginBottom = 100;
+  var randLeft = getRandomInt(container.offsetWidth - minMarginRight);
+  var randTop = getRandomInt(container.offsetHeight - minMarginBottom);
+  unsubscribe.style.left = randLeft + "px";
+  unsubscribe.style.top = randTop + "px";
+}
+
+function win () {
+  endGameMSG.style.display = "block";
+  setTimeout(endOfGame, 3000);
+
+}
+
+function endOfGame() {
+  unsubscribe.style.display = "none";
+  endGameMSG.style.display = "none";
+  playGame();
+}
+
+function playLevel2 () {
+  generateGridOfCheckBox();
+
+}
+
+function generateGridOfCheckBox() {
+  for(var i = 0; i < 3; i++ ){
+    for(var j= 0; j < 3; j++) {
+      var box = document.createElement("div");
+      box.classList.add('box');
+      console.log( i*_spacing);
+      box.style.left = i*_spacing + "px";
+      box.style.top = j*_spacing + "px";
+      container.appendChild(box);
+    }
+  }
 }
