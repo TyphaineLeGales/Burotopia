0640d708fa149d00c57bfd6e853bd9a06c746cac
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-05-19 11:58:05 +0200

    roll on btn clock

--- level5/jackpot.js
+++ level5/jackpot.js
@@ -5,9 +5,11 @@ var startUI = document.querySelector(".introLevel");
 var container = document.querySelector('div.gameContainer');
 var endGameMSG = document.querySelector('.endGameMSG');
 var machineGraphics = document.querySelector('.slotMachine');
+var btnStartMachine = document.querySelector('.btnStartMachine');
+btnStartMachine.addEventListener("click", startRound, false);
 
 var counter = 0;
-var slot_1 = document.getElementById('slot1');
+var slots = document.querySelectorAll('.slot');
 
 function clickHandler() {
 
@@ -18,18 +20,21 @@ function clickHandler() {
 function playGame() {
 
   machineGraphics.style.display = "block";
-  window.requestAnimationFrame(testScroll);
-
 }
 
 function testScroll () {
 
-  if(counter < slot_1.offsetHeight) {
+  if(counter < slots[0].offsetHeight) {
     counter += 1;
-  } else {
-    counter = 0;
+    window.requestAnimationFrame(testScroll);
   }
-  slot_1.scrollTop = counter;
-  console.log(slot_1.scrollTop );
+  for(var i = 0; i < slots.length; i++) {
+     slots[i].scrollTop = counter;
+
+  }
+}
+
+function startRound () {
+  counter = 0;
   window.requestAnimationFrame(testScroll);
 }
