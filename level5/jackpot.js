37f03785109cadfc818c0984168a93fe22c01a9e
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-05-19 11:44:28 +0200

    counter offsetHeight infinite loop

--- level5/jackpot.js
+++ level5/jackpot.js
@@ -6,6 +6,9 @@ var container = document.querySelector('div.gameContainer');
 var endGameMSG = document.querySelector('.endGameMSG');
 var machineGraphics = document.querySelector('.slotMachine');
 
+var counter = 0;
+var slot_1 = document.getElementById('slot1');
+
 function clickHandler() {
 
   playGame();
@@ -15,5 +18,19 @@ function clickHandler() {
 function playGame() {
 
   machineGraphics.style.display = "block";
+  window.requestAnimationFrame(testScroll);
 
+}
+
+function testScroll () {
+
+  if(counter < slot_1.offsetHeight) {
+    counter += 1;
+  } else {
+    console.log("branching works");
+    counter = 0;
+  }
+  console.log(counter);
+  slot_1.scrollTop = counter + "px";
+  window.requestAnimationFrame(testScroll);
 }
