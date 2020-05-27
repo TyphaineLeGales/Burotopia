91fe7de8f7cf758491f79327e921889b6326f8b0
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-05-27 15:56:20 +0200

    generate zip code box

--- level6/form.js
+++ level6/form.js
@@ -3,17 +3,39 @@ var gameContainer = document.querySelector('div.gameContainer');
 class Form {
   constructor() {
     this.container = document.createElement('div');
-    this.picture = document.createElement('div');
+    this.pictureBox = document.createElement('div');
+    this.nameBox = document.createElement('div');
+    this.zip = document.createElement('div');
     this.create();
   }
 
   create () {
     this.container = document.createElement('div');
     this.container.classList.add('formContainer');
-    this.picture.classList.add('picture');
 
-    this.container.appendChild(this.picture);
+    this.pictureBox.classList.add('picture');
+    this.pictureBox.classList.add('formItem');
+
+    this.nameBox.classList.add('name');
+    this.nameBox.classList.add('formItem');
+
+    this.zip.classList.add('zip');
+    this.createZipCode(this.zip);
+
+    this.container.appendChild(this.pictureBox);
+    this.container.appendChild(this.nameBox);
+    this.container.appendChild(this.zip);
 
     gameContainer.appendChild(this.container);
+  }
+
+  createZipCode (zipContainer) {
+    for(var i = 0; i< 6; i++) {
+      var box = document.createElement('div');
+      box.classList.add('zipBox');
+      box.classList.add('formItem');
+      box.style.left = i*2 +  "vw";
+      zipContainer.appendChild(box);
+    }
   }
 }
