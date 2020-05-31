0936398afa0d1cd27abcfcefa5bfef2573ffad9b
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-05-31 11:36:55 +0200

    generate sideItem at rand pos working

--- level6/formGame.js
+++ level6/formGame.js
@@ -4,7 +4,9 @@ button.addEventListener("click", clickHandler, false);
 var startUI = document.querySelector(".introLevel");
 var container = document.querySelector('div.gameContainer');
 var form;
-
+var offset = 50;
+var testBtn = document.querySelector('button.testBtn');
+testBtn.addEventListener('click', generateSideItems, false);
 function clickHandler() {
 
   playGame();
@@ -13,8 +15,28 @@ function clickHandler() {
 
 function playGame() {
   form = new Form();
+  // generateSideItems();
   // Generate empty form
   // Generate side items
   // Dragable item
   // Check correct position
 }
+
+function generateSideItems () {
+var pictureItem = document.createElement('div');
+  pictureItem.classList.add('picture');
+  pictureItem.classList.add('sideItem');
+  container.appendChild(pictureItem);
+  generateRandPos(container, pictureItem);
+    console.log(pictureItem);
+  //generate rand position within game container
+  //add classlist picture
+  //dragable
+
+}
+
+function  generateRandPos (container, div) {
+
+    div.style.left = randomInRange(0, container.offsetWidth - offset) + "px";
+    div.style.top = randomInRange(0, container.offsetHeight) + "px";
+  }
--- math.js
+++ math.js
@@ -1,3 +1,7 @@
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
+
+function randomInRange(min, max) {
+  return(Math.floor((Math.random() * (max - min) + 1) + min));
+}
