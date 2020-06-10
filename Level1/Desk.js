6ad0462d8c127552cf8edb5ecbdbb93ac26475d6
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-06-10 11:56:32 +0200

    employee gray

--- Level1/Desk.js
+++ Level1/Desk.js
@@ -55,7 +55,7 @@ class Desk{
     this.employee.classList.add('employee');
     this.divDesk.appendChild(this.employee);
     var randImg = getRandomInt(4);
-    this.employee.style.backgroundImage = 'url(../Assets/Graphics/Level1/PersoGris'+ randImg +'.png)';
+    this.employee.style.backgroundImage = 'url(../Assets/Graphics/Level1/employee'+ randImg +'.png)';
     this.employee.style.backgroundRepeat = "no-repeat";
     this.textEmployee.classList.add('textEmployee');
     this.divDesk.appendChild(this.textEmployee);
